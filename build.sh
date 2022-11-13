59fa8bb929de73157dc8dd4ddd632bd832e3cb5f
Dan Finlay <dan@danfinlay.com>
2022-11-12 21:44:13 -0800

    format

--- build.sh
+++ build.sh
@@ -1,3 +1,6 @@
 #! /bin/bash
 
-IDF_PATH=$HOME/esp32/esp-idf DEBUGGER_PORT=/dev/cu.DM02INUR UPLOAD_PORT=cu.usbmodem1101 mcconfig -d -m -p esp32/esp32s3 -t xsbug
+IDF_PATH=$HOME/esp32/esp-idf \
+DEBUGGER_PORT=/dev/cu.DM02INUR \
+UPLOAD_PORT=cu.usbmodem101 \
+mcconfig -d -m -p esp32/esp32s3 -t xsbug
